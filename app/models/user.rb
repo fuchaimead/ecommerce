e650208a4b4f4f3a0be477784e1bc32c3fb8a007
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-12-01 17:06:08 -0700

    initial project

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_one :cart
+
+  def display_name
+    "#{self.first_name} #{self.last_name}"
+  end
+end
---
+++ db/migrate/20171201232235_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.float :price
+      t.belongs_to :cart, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
