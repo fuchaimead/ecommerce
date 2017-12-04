6ecb950dc1da1cbba8d2c649b027b49c7dc28e44
Mallory Overton <malloryoverton3@gmail.com>
2017-12-04 08:56:14 -0700

    more styling changes on item index and show

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20171203080213) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatar"
     t.string "photo"
     t.boolean "in_cart", default: false
   end
